552d661ba3cbf213769a1a80eb1b52699f965740
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-09-15 11:50:54 -0400

    Create t1p3.py

---
+++ t1p3.py
@@ -0,0 +1,31 @@
+""" Simple Guessing Game """
+
+# Created by: Jonathan Pasco-Arnone
+# Created on: September 2023
+
+import random
+
+def main():
+    # Generate a random number from 1 to 100 with step of 1
+    # ex: if step was 2 then valid numbers would be 1,3,5,etc.
+    random_num = random.randrange(1, 100, 1)
+    retry = True
+    while retry:
+        retry = False
+    try:
+        guess = int(input("Please guess a random number between 1 and 100:\n"))
+        if guess == random_num:
+            print("You guessed correctly!")
+        else:
+            difference = random_num - guess
+            # For absolute value
+            if difference < 0:
+                difference *= -1
+            print("You were", difference, "off")
+    except Exception:
+        print("Please input an actual number")
+        retry = True
+
+
+if __name__ == "__main__":
+    main()
